64c0cad83806d273857c3039cc164444e395605b
Anirudh B S <anirudhbs96@gmail.com>
2017-12-13 15:08:44 +0530

    Add data and CSS

--- src/scripts/6.js
+++ src/scripts/6.js
@@ -4,7 +4,8 @@ function toggleDisplay(id) {
   for (let ar_i = 0; ar_i < pages.length; ar_i++)
     pagesArray.push(pages[ar_i])
   const otherDivs = pagesArray.filter((current) => current.id !== id)
-
+  document.getElementById('banner').style.display = 'none'
+  // document.getElementById('banner').style.zIndex = '-1'
   otherDivs.map((current) => {
     current.style.display = 'none'
   })
