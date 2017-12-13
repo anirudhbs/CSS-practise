fa8ce64d0eb50af188c8f95dd13fc1a5b0b4d8f7
Anirudh B S <anirudhbs96@gmail.com>
2017-12-13 10:11:22 +0530

    Refactored sliding-animations

--- src/scripts/4.js
+++ src/scripts/4.js
@@ -1,7 +1,19 @@
 function toggleDisplay(id1, id2) {
+  console.log('hello')
   const current = document.getElementById(id1)
   const otherDiv = document.getElementById(id2)
-  current.style.display = 'block';
-  otherDiv.style.display = 'none';
-  document.getElementById(id1).classList.add(`${id1}New`);
+  current.style.display = 'block'
+  otherDiv.style.display = 'none'
+  document.getElementById(id1).classList.add(`${id1}New`)
 }
+
+// function showDiv(id1, id2) {
+//   const current = document.getElementById(id1)
+//   const otherDiv = document.getElementById(id2)
+//   current.style.display = 'block'
+//   otherDiv.style.display = 'none'
+//   document.getElementById(id1).classList.add(`${id1}New`)
+// }
+
+// const current = document.getElementById('temp')
+// current.addEventListener('click', showDiv('one', 'two'), false)
