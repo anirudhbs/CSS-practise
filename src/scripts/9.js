8e6c4307b9381aa6037205b04e323da840a527d0
Anirudh B S <anirudhbs96@gmail.com>
2017-12-14 15:01:47 +0530

    Make navbar responsive

--- src/scripts/9.js
+++ src/scripts/9.js
@@ -1,13 +1,11 @@
 function toggleDisplay(id) {
   const current = document.getElementById(id)
   const banner = document.getElementById('zero')
-    if(banner.style.width !== '0%') banner.style.width = '0%'
+  if (banner.style.width !== '0%') banner.style.width = '0%'
   const other = document.getElementsByClassName('single')
   const divArray = []
-  for(let ar_i = 0; ar_i< other.length; ar_i++) divArray.push(other[ar_i])
+  for (let ar_i = 0; ar_i < other.length; ar_i++) divArray.push(other[ar_i])
   const otherDivs = divArray.filter((current) => current.id !== id && !current.classList.contains('zero'))
-  console.log(otherDivs)
-  
   otherDivs.map((current) => {
     current.style.width = '0%'
   })
@@ -18,13 +16,21 @@ function toggleApply(id) {
   const current = document.getElementById(id)
   const other = document.getElementsByClassName('single')
   const divArray = []
-  for(let ar_i = 0; ar_i< other.length; ar_i++) divArray.push(other[ar_i])
+  for (let ar_i = 0; ar_i < other.length; ar_i++) divArray.push(other[ar_i])
   const otherDivs = divArray.filter((current) => current.id !== id)
-  console.log(otherDivs)
-  
   otherDivs.map((current) => {
     current.style.width = '0%'
   })
-
   current.style.width = '100%'
 }
+
+const m = document.querySelector(".navbar-short")
+m.addEventListener("click", function () {
+  console.log('clicked')
+  const a = document.getElementById("navbar")
+  if (a.classList.contains("active")) {
+    a.classList.remove("active")
+  } else {
+    a.classList.add("active")
+  }
+})
